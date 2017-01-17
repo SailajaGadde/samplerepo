aff2a42a76e46962295c4bccf202ea38feba7e62
Student <student@achiever131.local>
2017-01-17 02:12:48 -0800

    Initial Commit

---
+++ samplegitrepo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  samplegitrepo
+//
+//  Created by Student on 17/01/17.
+//  Copyright © 2017 ais. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ samplegitrepo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  samplegitrepo
+//
+//  Created by Student on 17/01/17.
+//  Copyright © 2017 ais. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
